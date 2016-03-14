b0bdf84082ca1f353e61756fd35b83ba3c608bcd
Mao Thygesen and Veronica Alley <veronica.aubry.alley@gmail.com>
2016-03-14 11:26:00 -0700

    add wordCount

--- js/journal.js
+++ js/journal.js
@@ -2,11 +2,11 @@
 exports.journal = function(title, text) {
   this.title = title;
   this.text = text;
+  this.wordCount = function() {
+    var count = this.text.split(" ");
+    return count.length;
+  }
 }
 
-// this.wordCount = function() {
-//   var count = this.text.split(" ");
-//   return count.length;
-// }
 
 // exports.module = Journal;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,14 +1,13 @@
-var journal = require('./../js/journal.js').Journal;
+var journal = require('./../js/journal.js').journal;
 
 $(document).ready(function(){
   $('#journal').submit(function(event){
     event.preventDefault();
     var title = $('#title').val();
     var text = $('#text').val();
-    // var entry = new Journal(title, text);
-    $('.postTitle').append("<h2>" + title + "</h2>");
-    console.log ("alert");
-    $('.post').append("<p>" + text + "</p>");
+    var entry = new journal(title, text);
+    $('.posts').append("<div class ='postEntry'>" + "<h2>" + title + "</h2>" + "<p>" + text + "</p>" + entry.wordCount() + "</div>");
+    $("#journal")[0].reset();
     });
   });
 
@@ -19,11 +18,11 @@ $(document).ready(function(){
 exports.journal = function(title, text) {
   this.title = title;
   this.text = text;
+  this.wordCount = function() {
+    var count = this.text.split(" ");
+    return count.length;
+  }
 }
 
-// this.wordCount = function() {
-//   var count = this.text.split(" ");
-//   return count.length;
-// }
 
 // exports.module = Journal;
